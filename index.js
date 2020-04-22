e8b3971b2a54eedf18d185a2828dab40d0fcbeaa
vitamaxDH <hellonewtry@gmail.com>
2020-04-22 11:15:17 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,13 +2,33 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const mongoose = require('mongoose');
-const db = "mongodb+srv://max:1234@test-cluster1-eyswr.mongodb.net/test?retryWrites=true&w=majority";
-mongoose.connect(db,{useNewUrlParser:true, useUnifiedTopology:true})
+const bodyParser = require('body-parser');
+const User = require('./models/User');
+
+const config = require('./config/key');
+
+mongoose.connect(config.mongoURI,{useNewUrlParser:true, useUnifiedTopology:true})
         .then(()=>console.log(`DB Connected`))
         .catch(err=>console.log(err));
 
+//application/x-www/form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
 app.get('/', (req, res)=>{
-    res.send('hello world!');
+    res.send('반가워유~~~~~~~~');
+})
+
+app.post('/register', (req, res)=>{
+
+    const user = new User(req.body);
+    user.save((err,userInfo)=>{
+        if(err) return res.json({success:false, err});
+        return res.status(200).json({
+            success: true
+        });
+    })
+
 })
 
 app.listen(port, ()=>console.log(`Server is on port ${port}`));
